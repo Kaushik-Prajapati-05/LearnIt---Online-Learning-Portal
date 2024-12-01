bfe782e77b7cf4bc864a198ddbb0fa2f52af9e79
neeltandel <122268045+neeltandel@users.noreply.github.com>
2024-12-02 01:55:12 +0530

    Update course-routes.js

--- backend/Routes/student-routes/course-routes.js
+++ backend/Routes/student-routes/course-routes.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const getCourseDetails = require("../../controllers/student-controller/course-controller");
+const {buyNow} = require("../../controllers/student-controller/buynow-controller");
+const {listCourses} = require("../../controllers/student-controller/list-courses");
 const router = express.Router();
 
 router.route('/:id').get(getCourseDetails.getStudentViewCourseDetails);
+router.route('/buy').post(buyNow);
+router.route('/listcourses').post(listCourses);
+
 
 module.exports = router;
