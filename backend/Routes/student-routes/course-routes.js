8fa0c10b22011e333eb76779673c835e5fc15f0a
202201524 <202201524@daiict.ac.in>
2024-12-06 23:41:44 +0530

    route setup for quiz

--- backend/Routes/student-routes/course-routes.js
+++ backend/Routes/student-routes/course-routes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const getCourseDetails = require("../../controllers/student-controller/course-controller");
 const {buyNow} = require("../../controllers/student-controller/buynow-controller");
 const {listCourses} = require("../../controllers/student-controller/List-course");
+const {getQuizzesByCourseId} = require("../../controllers/student-controller/quiz-controller");
 const router = express.Router();
 
 router.route('/:id').get(getCourseDetails.getStudentViewCourseDetails);
 router.route('/buy').post(buyNow);
 router.route('/listcourses').post(listCourses);
+router.route('/quiz/:id').get(getQuizzesByCourseId);
 
 
 module.exports = router;
